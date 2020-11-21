b8758f8d0030c14badd31cd2af4c17655cb12592
Jon Peter Pomperada <jonpeterpomperada@gmail.com>
2020-11-22 00:34:07 +0800

    cliclabke check box

--- popup.js
+++ popup.js
@@ -34,7 +34,25 @@ function testingButton(){
     chrome.tabs.insertCSS(null, { file: "bbl_dark_mode.css" });
     console.log("Button is Pressed");
 }
+
+function restoreOptions() {
+    // Use default value = false.
+    chrome.storage.sync.get({
+        value: false
+    }, function (items) {
+        document.getElementById('darkSwitch').checked = items.value;
+    });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    restoreOptions();
+    document.getElementById("darkSwitch").addEventListener('click', hello);
+    alert("DOM Loaded");
+});
+}
 */
+
+var x;
 
 const urls = ["https://mcl.blackboard.com/ultra/institution-page",
 "https://mcl.blackboard.com/ultra/profile",
@@ -51,43 +69,49 @@ function testingButton(){
     alert('turning to dark mode');
 }
 
+
+
+
 function hello() {
+    checkCheckbox();
+
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let currentUrl = tabs[0].url;
-        // use `url` here inside the callback because it's asynchronous!
-        for(i = 0; i < urls.length; i++){
-            if(currentUrl == urls[i]){
-                chrome.tabs.insertCSS(null, { file: "bbl_dark_mode.css" });
-                alert('turning to dark mode');
-                break;
-            }
-            }
+        
         alert('status: ' + x);
+
+        if(x){
+            // use `url` here inside the callback because it's asynchronous!
+            for(i = 0; i < urls.length; i++){
+                if(currentUrl == urls[i]){
+                    chrome.tabs.insertCSS(null, { file: "bbl_dark_mode.css" });
+                    //alert('turning to dark mode');
+                    break;
+                }
+            }
+        }else{
+            
+        }
+
 
     });
 
     
   }
 
+  function removeCSS(file) {
+    const cssNode = document.getElementById(file)
+    cssNode && cssNode.parentNode.removeChild(cssNode)
+  }
+
   // Restores checkbox state using the preferences stored in chrome.storage.sync
-function restoreOptions() {
-    // Use default value = false.
-    chrome.storage.sync.get({
-        value: false
-    }, function (items) {
-        document.getElementById('darkSwitch').checked = items.value;
-    });
+
+ 
+function checkCheckbox() {
+    x = document.getElementById("darkSwitch").checked;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    restoreOptions();
-    document.getElementById("darkSwitch").addEventListener('click', hello);
-    alert("DOM Loaded");
-});
- 
-
-//var x = document.getElementById("darkSwitch").checked;
-var x = document.querySelector('.messageCheckbox').checked;
+document.getElementById('darkSwitch').addEventListener('click', hello);
 document.getElementById('hello').addEventListener('click', hello);
   
 
