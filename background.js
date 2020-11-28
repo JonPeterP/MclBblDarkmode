57aa9bea286ce5e2a66d547c593a46a1566541e3
Jon Peter Pomperada <jonpeterpomperada@gmail.com>
2020-11-28 23:45:26 +0800

    Working popup.js

--- background.js
+++ background.js
@@ -39,8 +39,9 @@ manifest code fore background
 //THIS CAN ALSO CALL THE CONTENT SCRIPT AND STUFF
 //, "default_popup": "popup.html"
 
-let state = false;
+//let state = false;
 
+/*
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(tab){
@@ -63,9 +64,74 @@ function buttonClicked(tab){
   //chrome.tabs.insertCSS(null, { file: "light_mode.css" });
   chrome.tabs.sendMessage(tab.id, normal);
   state = !state;
-
-
-
-
 }
+*/
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  //restoreOptions();
+  //loadIfDarkmode();
+
+ 
+  console.log("BACKGROUND DOM Loaded");
+});
+
+
+
+let state = false;
+
+function restoreOptions() {
+  // Use default value = false.
+  chrome.storage.sync.get({
+      switchValue: true
+  }, function (items) {
+      state = items.switchValue;
+      //document.getElementById('darkSwitch').checked = items.switchValue;
+      //console.log("Item Value: " + items.switchValue);
+  });
+}
+
+function loadIfDarkmode(){
+
+  let param = {
+    active: true,
+    currentWindow: true
+  }
+  chrome.tabs.query(param, gotTabs);
+
+    function gotTabs(tab){
+      let msg = {
+        txt: "off"
+      }
+
+      let normal = {
+        txt: "on"
+      }
+      //chrome.tabs.sendMessage(tab.id, msg);
+      //console.log(tab);
+
+      if (!state) {
+        //chrome.tabs.insertCSS(null, { file: "bbl_dark_mode.css" });
+        chrome.tabs.sendMessage(tab[0].id, msg);
+        state = !state;
+        //saveOptions();
+        return;
+      }
+      //chrome.tabs.insertCSS(null, { file: "light_mode.css" });
+      chrome.tabs.sendMessage(tab[0].id, normal);
+      state = !state;
+
+      //console.log()
+
+    //  saveOptions();
+  }
+}
+
+
+/*
+"background": {
+    "scripts": ["background.js"]
+  },
+*/
 
--- popup.js
+++ popup.js
@@ -1,201 +1,90 @@
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    var btn = document.getElementById("testing");
-    console.log("This is the thing: " + btn);
-    // onClick's logic below:
-    btn.addEventListener("click", function() {
-        testingButton();
-    });
-});
+function setup(){
+  noCanvas();
+  //let userput = select
 
-
-
-document.addEventListener('DOMContentLoaded', function (event) {
-
-        // it should work
-    var darkSwitch = document.getElementById("darkSwitch");
-    darkSwitch.addEventListener('click', testingButton, false);
-    console.log("DOM Loaded");
-    });
-
-    
-function myFunction(){
-    var x = document.getElementById("darkSwitch").nodeValue;
-    console.log(x);
 }
 
 
-function btnFunction(){
-    var x = document.getElementById("darkSwitch").nodeValue;
-    console.log(x);
-}
 
-function testingButton(){
-    chrome.tabs.insertCSS(null, { file: "bbl_dark_mode.css" });
-    console.log("Button is Pressed");
-}
 
-function restoreOptions() {
-    // Use default value = false.
-    chrome.storage.sync.get({
-        value: false
-    }, function (items) {
-        document.getElementById('darkSwitch').checked = items.value;
-    });
+
+let state = false;
+
+//chrome.browserAction.onClicked.addListener(buttonClicked);
+
+//document.getElementById('darkSwitch').onclick = buttonClicked;
+
+function buttonClicked(switchValue){
+
+  let param = {
+    active: true,
+    currentWindow: true
+  }
+  chrome.tabs.query(param, gotTabs);
+  saveOptions();
+    function gotTabs(tab){
+      let msg = {
+        txt: "on"
+      }
+
+      let normal = {
+        txt: "off"
+      }
+   
+
+      if (state) {
+      
+        chrome.tabs.sendMessage(tab[0].id, normal);
+        state = !state;
+        //saveOptions();
+        return;
+      }else{
+  
+        chrome.tabs.sendMessage(tab[0].id, msg);
+        state = !state;
+       
+      }
+     
+
+      //console.log()
+
+   
+  }
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    restoreOptions();
-    document.getElementById("darkSwitch").addEventListener('click', hello);
-    alert("DOM Loaded");
+  restoreOptions();
+  
+ 
+  //loadIfDarkmode();
+  document.getElementById("darkSwitch").addEventListener('click', buttonClicked);
+  
+ 
+  console.log("DOM Loaded");
 });
+
+// Restores checkbox state using the preferences stored in chrome.storage.sync
+function restoreOptions() {
+  // Use default value = false.
+  chrome.storage.sync.get({
+      switchValue: true
+  }, function (items) {
+      state = items.switchValue;
+      document.getElementById('darkSwitch').checked = items.switchValue;
+      //console.log("Item Value: " + items.switchValue);
+  });
 }
 
+function saveOptions(){
+    var darkMode = document.getElementById('darkSwitch').checked;
 
-
-
-//SOME SHITS
-import browser from 'webextension-polyfill'
-
-function insertCSS(file) {
-  const style = document.createElement('link')
-  style.rel = 'stylesheet'
-  style.type = 'text/css'
-  style.href = browser.extension.getURL('bbl_dark_mode.css')
-  style.id = file
-  document.getElementsByTagName('html')[0].appendChild(style)
-}
-
-function removeCSS(file) {
-  const cssNode = document.getElementById(file)
-  cssNode && cssNode.parentNode.removeChild(cssNode)
-}
-
-browser.runtime.onMessage.addListener(message => {
-  const id = 'redact-the-web'
-  if (message.command === 'insertCSS') {
-    insertCSS(id)
-  } else if (message.command === 'removeCSS') {
-    removeCSS(id)
-  }
-})
-*/
-
-var switchStatus;
-
-var cssFile = "bbl_dark_mode";
-
-const urls = ["https://mcl.blackboard.com/ultra/institution-page",
-"https://mcl.blackboard.com/ultra/profile",
-"https://mcl.blackboard.com/ultra/stream",
-"https://mcl.blackboard.com/ultra/course",
-"https://mcl.blackboard.com/ultra/organization",
-"https://mcl.blackboard.com/ultra/calendar",
-"https://mcl.blackboard.com/ultra/messages",
-"https://mcl.blackboard.com/ultra/grades",
-"https://mcl.blackboard.com/ultra/tools"];
-
-function testingButton(){
-    chrome.tabs.insertCSS(null, { file: "bbl_dark_mode.css" });
-    alert('turning to dark mode');
-}
-
-
-
-
-function hello() {
-    checkCheckbox();
-
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-      
-        let currentUrl = tabs[0].url;
-        
-        //console.log(currentUrl);
-        alert('status: ' +  currentUrl);
-
-        if(switchStatus){
-            // use `url` here inside the callback because it's asynchronous!
-            for(i = 0; i < urls.length; i++){
-               // if(currentUrl == urls[i]){
-                    //chrome.tabs.insertCSS(null, { file: "bbl_dark_mode.css" });
-                    //alert('turning to dark mode');
-                    
-                    //loadCSS("bbl_dark_mode.css");
-                    addCss('bbl_dark_mode.css');
-                    
-                    break;
-             //  }
-            }
-
-           
-        }else{
-          //turn to light mode here
-
-            //chrome.tabs.insertCSS(null, { file: "light_mode.css" });
-          //  document.getElementById("body").classList.remove('JMaylinCustomStyles');
-        }
-
-
+    chrome.storage.sync.set({
+        switchValue: darkMode
     });
-
-    
-  }
-
-  
-
- function addCss(fileName) {  
-
-    var head = document.head;
-    var link = document.createElement("link");
-  
-    link.id = fileName;
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = chrome.extension.getURL(fileName);
-  
-    //alert(link);
-    head.appendChild(link);
-    //document.getElementsByTagName("html")[0].appendChild(link);
-   
-  }
-
-  function removeCSS(fileName) {
-    var cssNode = document.getElementById(fileName);
-    cssNode && cssNode.parentNode.removeChild(cssNode);
-  }
-
-
-
-  // Restores checkbox state using the preferences stored in chrome.storage.sync
-
- 
-function checkCheckbox() {
-    switchStatus = document.getElementById("darkSwitch").checked;
 }
 
-console.log("Content Script Here");
-//addCss("bbl_dark_mode.css");
-
-var switchh = document.getElementById('darkSwitch');
-if(switchh){
-  switchh.addEventListener('click', hello);
-}
-
-//document.getElementById('darkSwitch').addEventListener('click', hello);
-//document.getElementById('hello').addEventListener('click', hello);
- 
 
 
-chrome.runtime.onMessage.addListener(gotMessage);
 
-
-function gotMessage(message, sender, sendResponse){
-  console.log(message.txt);
-
-  if(message.txt === "hello"){
-    addCss("bbl_dark_mode.css");
-  }else if(message.txt === "hi"){
-    removeCSS("bbl_dark_mode.css");
-  }
-}
 
